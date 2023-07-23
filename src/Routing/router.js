438630ccb0e229c49c3df246d79e83ca8e62b5c4
julasweta <julasweta@ukr.net>
2023-07-23 14:13:44 +0300

    hw-6-routing

--- src/Routing/router.js
+++ src/Routing/router.js
@@ -1,11 +1,10 @@
 import { createBrowserRouter, Outlet, useNavigate, useLocation, useParams } from "react-router-dom";
-import Users from "../pages/users/Users"
 import { AppRoutes } from "./AppRoutes";
 
 
 export const router = createBrowserRouter([
     {
-        element: <Users/>,
+        element: <>Pruvit</>,
         errorElement: <h1>OOOOOpppsie</h1>,
         children: [
           {
