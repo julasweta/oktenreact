aa7c8223984e0dcc0fcd3f2024919d53b965f130
julasweta <julasweta@ukr.net>
2023-07-27 22:36:17 +0300

    add: lightning

--- src/pages/movies/MoviesPage.js
+++ src/pages/movies/MoviesPage.js
@@ -1,17 +1,15 @@
-
-import '../../App.css';
-import { MoviesList } from '../../components';
-
+import "../../App.css";
+import { MoviesList } from "../../components";
 
 const MoviesPage = () => {
-
   return (
-    <div className='container'>
-      <div className='movies'>
+    <div className="container">
+      <div class="lightning"></div>
+      <div className="movies">
         <MoviesList />
       </div>
     </div>
   );
 };
 
-export  {MoviesPage};
+export { MoviesPage };
