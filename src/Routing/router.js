f79b7c6aa18e308e044935bcb5aa7fd1f5b28fe2
julasweta <julasweta@ukr.net>
2023-08-20 02:58:30 +0300

    hw-12-redux-toolkit

--- src/Routing/router.js
+++ src/Routing/router.js
@@ -11,7 +11,7 @@ export const router = createBrowserRouter([
         children: [
           {
             index: true,
-            element: <Navigate to={'characters'}/>,
+            element: <Navigate to={'episodes'}/>,
           },
           {
             path: AppRoutes.EPISODES,
