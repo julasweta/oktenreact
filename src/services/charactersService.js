5901c9e1f969499e28a26f3e2fb88bdaab50f15f
julasweta <julasweta@ukr.net>
2023-08-20 03:00:31 +0300

    hw-12-redux-toolkit

--- src/services/charactersService.js
+++ src/services/charactersService.js
@@ -3,21 +3,6 @@ import { baseUrl, urls } from "../constants/urls";
 import { apiService } from "./apiService";
 
 
-//  в функцію передаємо дані, які нам потрібно для створення повної адреси, без базової, базова в exampleApiService
-
-/*
-// Приклад використання на сторінці
-
-             carsService.getAll().then(({ data }) => dispatch(carsActions.setCars(data)));
-// Другий приклад
-
-    carsService.changeCar(data2, car[0].id).then(({ data }) => {
-            dispatch(carsActions.deleteTriger())
-            dispatch(carsActions.setShowForm('add'))
-        }) 
-
-  */
-
 const charactersService = {
     getAll: () => apiService.get(baseUrl+urls.character),
     getById: (url)=> apiService(url),
