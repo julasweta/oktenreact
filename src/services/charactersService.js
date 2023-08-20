dc075f420ef0c69a1ee62fccf4c785012aee79e7
julasweta <julasweta@ukr.net>
2023-08-20 03:01:14 +0300

    hw-12-redux-toolkit

--- src/services/charactersService.js
+++ src/services/charactersService.js
@@ -6,9 +6,6 @@ import { apiService } from "./apiService";
 const charactersService = {
     getAll: () => apiService.get(baseUrl+urls.character),
     getById: (url)=> apiService(url),
-   /*  addCar: (data) => exampleApiService.post(urls.owu.cars, data),
-    deleteCar: (id) => exampleApiService.delete(urls.owu.cars + `/${id}`),
-    changeCar: (data, id) => exampleService.put(urls.owu.cars + `/${id}`, data), */
 }
 
 export {
